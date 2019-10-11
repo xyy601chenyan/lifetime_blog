836fee33621ac699a840e76ebc9b63d16072fab7
xyy601chenyan <xyy545688506@gmail.com>
2019-10-11 12:59:55 +0800

    user can login/logout

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'jquery-ui-rails'
 
 gem 'jquery-rails'
 
+gem 'devise'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use mysql as the database for Active Record
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!,only: [:new,:create,:edit,:update,:destroy]
   before_action :find_article,only: [:show,:edit,:update,:destroy]
 
   def index
