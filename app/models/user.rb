3474f4259b2eed600c4336890f0f6ae8f806e7d6
xyy601chenyan <xyy545688506@gmail.com>
2019-10-12 18:47:52 +0800

    hide sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # 删除:registerable模块，以取消注册功能
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+  #devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :validatable
 
   has_many :articles
 end
