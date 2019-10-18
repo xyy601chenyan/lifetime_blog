09f0f957dbfca7a5b5975c2463113378cd19745f
xyy601chenyan <xyy545688506@gmail.com>
2019-10-18 12:05:41 +0800

    show random picture when enter welcome page

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,17 @@
 module WelcomeHelper
+
+  def random_background_image
+    #file_names = Dir.glob(Rails.root.join("public/background/*").to_s)
+    #file_names = Dir.entries("app/assets/images")
+    file_names = Dir.entries("public/background")
+    useless_lists = [".","..",".keep",".DS_Store"]
+
+    useless_lists.each do |list|
+      file_names.delete(list)
+    end
+    random_image = file_names.sample
+    return "background/#{random_image}"
+
+
+  end
 end
